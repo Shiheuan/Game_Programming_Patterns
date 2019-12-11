ff921f73083167f19b8afa7645d879622e08fe45
shiheuan <shiheuan@gmail.com>
2019-12-11 14:14:55 +0800

    #4. Use 'Event' in C# refine Observer Pattern Example.

---
+++ Assets/Patterns/ObserverPattern/Source/AchievementSystemWithEvents.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace ObserverPattern
+{
+    public class AchievementSystemWithEvents : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            PlayerPrefs.DeleteAll();
+
+            PointOfInterestWithEvents.OnPointOfInterestEntered += PointOfInterestWithEvents_OnPointOfInterestEntered;
+        }
+
+        private void PointOfInterestWithEvents_OnPointOfInterestEntered(PointOfInterestWithEvents poi)
+        {
+            string achievementKey = "achievement-" + poi.PoiName;
+            if (PlayerPrefs.GetInt(achievementKey) == 1)
+                return;
+
+            PlayerPrefs.SetInt(achievementKey, 1);
+            Debug.Log("Unlocked " + poi.PoiName);
+        }
+
+    }
+
+}
+
---
+++ Assets/Patterns/ObserverPattern/Source/PointOfInterestWithEvents.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace ObserverPattern
+{
+    public class PointOfInterestWithEvents : MonoBehaviour
+    {
+        public static event Action<PointOfInterestWithEvents> OnPointOfInterestEntered;
+
+        [SerializeField]
+        private string _poiName;
+
+        public string PoiName { get { return _poiName;} }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //OnPointOfInterestEntered?.Invoke(this);
+            if (OnPointOfInterestEntered != null)
+            {
+                OnPointOfInterestEntered(this);
+            }
+        }
+
+    }
+
+
+}
